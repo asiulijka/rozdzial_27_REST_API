bac08d270a2f8adf7d6dc34e625c90514faff816
Joanna Zielinska <j.m.h.zielinska@gmail.com>
2021-10-20 23:18:03 +1100

    27.6 to push to heroku

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-// const router = express.Router();
-
-let db = require('./db');
-
-const app = express();
+const path = require('path');
 
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 const testimonialsRoutes = require('./routes/testimonials.routes');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -17,12 +16,17 @@ app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes); 
 app.use('/api', testimonialsRoutes); 
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.use((req, res) => {
   res.status(404).send({message: 'Not found...'});
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
